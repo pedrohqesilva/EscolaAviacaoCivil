27b82c00c15d775b5cd11986970271df5a30a80a
PEDRO HENRIQUE DE ALMEIDA E SILVA <pedro.almeida@tce.mg.gov.br>
2019-05-22 11:48:15 -0300

    Add project files.

---
+++ ClassLibrary1/Entities/Aviao.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities
+{
+    public class Aviao : BaseEntity
+    {
+        public string Matricula { get; private set; }
+
+        private Aviao()
+        {
+            Metadata = Metadata.Create();
+        }
+
+        public static Aviao Create(string matricula) => new Aviao { Matricula = matricula };
+
+        public Aviao UpdateMatricula(string matricula)
+        {
+            Metadata.Update();
+
+            Matricula = matricula;
+            return this;
+        }
+    }
+}
---
+++ ClassLibrary1/Entities/Base/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Domain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public Metadata Metadata { get; set; }
+    }
+
+    public class Metadata
+    {
+        public DateTime DataCriacao { get; private set; }
+        public DateTime? DataAtualizacao { get; private set; }
+        public DateTime? DataRemocao { get; private set; }
+
+        public bool IsRemovido { get; private set; }
+
+        public static Metadata Create() => new Metadata { DataCriacao = DateTime.Now };
+
+        public Metadata Update()
+        {
+            DataAtualizacao = DateTime.Now;
+            return this;
+        }
+
+        public Metadata Remove()
+        {
+            DataRemocao = DateTime.Now;
+            IsRemovido = true;
+            return this;
+        }
+    }
+}
