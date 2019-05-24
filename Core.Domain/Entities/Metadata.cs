38495060eaab4862f1f68024fcf9e1d352379cfe
Pedro Henrique <pedrohqesilva@gmail.com>
2019-05-24 00:56:04 -0300

    [Core] [Domain] Ajustes

--- Core.Domain/Entities/Metadata.cs
+++ Core.Domain/Entities/Metadata.cs
@@ -4,7 +4,7 @@ namespace Core.Domain.Entities
 {
     public class Metadata
     {
-        public DateTime DataCriacao { get; private set; } = DateTime.Now;
+        public DateTime DataCriacao { get; } = DateTime.Now;
         public DateTime? DataModificacao { get; private set; }
         public DateTime? DataExclusao { get; private set; }
         public bool IsExcluido { get; private set; }
