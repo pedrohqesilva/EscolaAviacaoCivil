692746052c9351e2c0c3e5621d2b41a91a3d58fc
Pedro Henrique <pedrohqesilva@gmail.com>
2019-05-24 01:29:47 -0300

    [Enums] Ajuste e inclusão de novos

---
+++ Aeroportos.Domain/Enums/TiposAeroportos.cs
@@ -0,0 +1,9 @@
+﻿namespace Aeroportos.Domain.Enums
+{
+    public static class TiposAeroportos
+    {
+        public const string Aeroporto = "AP";
+        public const string Aerodromo = "AD";
+        public const string Heliponto = "HP";
+    }
+}
